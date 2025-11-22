cbf4e0e4e324586653307ce19c72a6dff86a578e
Jose Galindo <jgalindo@tse.org.gt>
2025-11-22 07:15:15 -0600

    first commit

---
+++ src/main/java/com/example/pruebachn/PruebachnApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebachn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebachnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebachnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pruebachn/PruebachnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebachn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebachnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
